15f2d6659dbe565b80bff62a9088cfaecb2f25ff
matty1979 <47564833+matty1979@users.noreply.github.com>
2019-07-02 09:13:35 -0400

    Update s3download.sh
    fixed path of create cluster declare

--- s3download.sh
+++ s3download.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -e
-mkdir ~/tmp
-cd ~/tmp
-aws s3 sync s3://octo-openshift ~/tmp
+mkdir /home/cloud_user/ocp41
+chown -R cloud_user:cloud_user /home/cloud_user/ocp41
+cd /home/cloud_user/ocp41
+aws s3 sync s3://octo-openshift /home/cloud_user/ocp41
 sleep 5
-tar xvf ~/tmp/openshift-install-linux-4.1.3.tar.gz
-tar xvf ~/tmp/openshift-client-linux-4.1.3.tar.gz
-./openshift-install create cluster --dir=~/tmp/install-cluster.yaml \ --log-level debug
+tar xvf openshift-install-linux-4.1.3.tar.gz
+tar xvf openshift-client-linux-4.1.3.tar.gz
+./openshift-install create cluster --dir=/home/cloud_user/ocp41 --log-level debug
